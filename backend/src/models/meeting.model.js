d05d05364d6b3398589d33cd9883e66974414950
Rishabh6353 <rishabhoop@gmail.com>
2025-05-20 21:44:34 +0530

    backend structure of my project

---
+++ backend/src/models/meeting.model.js
@@ -0,0 +1,11 @@
+import mongoose,{Schema} from "mongoose";
+
+const meetingSchema = new Schema({
+    user_id: {type:String},
+    mettingCode:{type:String, required:true},
+    date:{type:Date, default:Date.now, required:true}
+});
+
+const Meeting = new model("Meeting","meetingSchema");
+
+export {Meeting};
